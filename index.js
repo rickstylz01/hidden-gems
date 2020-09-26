7163ca9edf3c633e6766e5c15d5b581e7295607f
Ricardo Maya <rickmaya@gmail.com>
2020-09-26 14:14:57 -0500

    add favicon and try to add google local

--- index.js
+++ index.js
@@ -1,58 +1,83 @@
-'use strict';
-
-// Initialize and add the map
-var infowindow;
 function initMap() {
-  const mapCenter = { lat: 41.8781, lng: -87.6298 };
-  
-  let map = new google.maps.Map(document.getElementById('map'), {
-    center: mapCenter,
-    zoom: 10
+  const myCoordinates = { lat: 41.878113, lng: -87.629799 };
+
+  const lcMapView = new google.maps.localContext.LocalContextMapView({
+    element: document.querySelector('#map'),
+    placeTypePreferences: [
+      'bakery', 
+      'cafe', 
+      'restaurant', 
+      'tourist_attraction'
+    ],
+    maxPlaceCount: 24,
   });
 
-  infowindow = new google.maps.InfoWindow();
-  
-  var marker = new google.maps.Marker({
-    position: mapCenter,
-    map: map,
-    animation: google.maps.Animation.DROP
+  const map = lcMapView.map;
+  map.setOptions({
+    center: myCoordinates,
+    zoom: 16
   });
-    
-  function drop() {
-    for (var i = 0; i < markerArray.length; i++) {
-      setTimeout(function () {
-        addMarkerMethod();
-      }, i * 200);
+
+  const card = document.getElementById("pac-card");
+
+  const input = document.getElementById("pac-input");
+
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+  const autocomplete = new google.maps.places.Autocomplete(input);
+
+  // Bind the map's bounds (viewport) property to the autocomplete object,
+  // so that the autocomplete requests use the current map bounds for the
+  // bounds option in the request.
+  autocomplete.bindTo("bounds", map);
+  // Set the data fields to return when the user selects a place.
+  autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
+  const infowindow = new google.maps.InfoWindow();
+  const infowindowContent = document.getElementById("infowindow-content");
+  infowindow.setContent(infowindowContent);
+  const marker = new google.maps.Marker({
+    map,
+    anchorPoint: new google.maps.Point(0, -29),
+  });
+  autocomplete.addListener("place_changed", () => {
+    infowindow.close();
+    marker.setVisible(false);
+    const place = autocomplete.getPlace();
+
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
     }
-  } 
-}
 
-// Initialize and add autocomplete 
-let autocomplete;
-function initAutocomplete() {
-  autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),
-    {
-      types: ['establishment'],
-      componentRestrictions: { 'country': ['USA'] },
-      fields: ['place_id', 'geometry', 'name']
-    });
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17); // Why 17? Because it looks good.
+    }
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+    let address = "";
 
-  // When the user selects an address from the dropdown, populate the address fields in the form.
-  autocomplete.addListener('place_changed', onPlaceChanged);
-}
-
-function onPlaceChanged() {
-  var place = autocomplete.getPlace();
-  if (!place.geometry) {
-    //User did not select a prediction; reset the input field
-    document.getElementById('autocomplete').placeholder = 'Enter a place';
-  }  else {
-    //Display details about the valid place 
-    document.getElementById('details').innerHTML = place.name;
-  }
-}
-
-function initialize() {
-  initMap();
-  initAutocomplete();
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] &&
+          place.address_components[0].short_name) ||
+        "",
+        (place.address_components[1] &&
+          place.address_components[1].short_name) ||
+        "",
+        (place.address_components[2] &&
+          place.address_components[2].short_name) ||
+        "",
+      ].join(" ");
+    }
+    infowindowContent.children["place-icon"].src = place.icon;
+    infowindowContent.children["place-name"].textContent = place.name;
+    infowindowContent.children["place-address"].textContent = address;
+    infowindow.open(map, marker);
+  });
 }
