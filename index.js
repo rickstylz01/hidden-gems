41664fcbb9d2c2e41fe15986c1a94b3bbeae341a
Ricardo Maya <rickmaya@gmail.com>
2020-09-27 11:24:26 -0500

    adjust with of search bar

--- index.js
+++ index.js
@@ -1,136 +1,196 @@
+let map;
+const silverStyle = [{
+  "elementType": "geometry",
+  "stylers": [{
+    "color": "#f5f5f5"
+  }]
+}, {
+  "elementType": "labels",
+  "stylers": [{
+    "visibility": "off"
+  }]
+}, {
+  "elementType": "labels.icon",
+  "stylers": [{
+    "visibility": "off"
+  }]
+}, {
+  "elementType": "labels.text.fill",
+  "stylers": [{
+    "color": "#616161"
+  }]
+}, {
+  "elementType": "labels.text.stroke",
+  "stylers": [{
+    "color": "#f5f5f5"
+  }]
+}, {
+  "featureType": "administrative.land_parcel",
+  "stylers": [{
+    "visibility": "off"
+  }]
+}, {
+  "featureType": "administrative.land_parcel",
+  "elementType": "labels.text.fill",
+  "stylers": [{
+    "color": "#bdbdbd"
+  }]
+}, {
+  "featureType": "administrative.neighborhood",
+  "stylers": [{
+    "visibility": "off"
+  }]
+}, {
+  "featureType": "poi",
+  "elementType": "geometry",
+  "stylers": [{
+    "color": "#eeeeee"
+  }]
+}, {
+  "featureType": "poi",
+  "elementType": "labels.text.fill",
+  "stylers": [{
+    "color": "#757575"
+  }]
+}, {
+  "featureType": "poi.park",
+  "elementType": "geometry",
+  "stylers": [{
+    "color": "#e5e5e5"
+  }]
+}, {
+  "featureType": "poi.park",
+  "elementType": "labels.text.fill",
+  "stylers": [{
+    "color": "#9e9e9e"
+  }]
+}, {
+  "featureType": "road",
+  "elementType": "geometry",
+  "stylers": [{
+    "color": "#ffffff"
+  }]
+}, {
+  "featureType": "road.arterial",
+  "elementType": "labels.text.fill",
+  "stylers": [{
+    "color": "#757575"
+  }]
+}, {
+  "featureType": "road.highway",
+  "elementType": "geometry",
+  "stylers": [{
+    "color": "#dadada"
+  }]
+}, {
+  "featureType": "road.highway",
+  "elementType": "labels.text.fill",
+  "stylers": [{
+    "color": "#616161"
+  }]
+}, {
+  "featureType": "road.local",
+  "elementType": "labels.text.fill",
+  "stylers": [{
+    "color": "#9e9e9e"
+  }]
+}, {
+  "featureType": "transit.line",
+  "elementType": "geometry",
+  "stylers": [{
+    "color": "#e5e5e5"
+  }]
+}, {
+  "featureType": "transit.station",
+  "elementType": "geometry",
+  "stylers": [{
+    "color": "#eeeeee"
+  }]
+}, {
+  "featureType": "water",
+  "elementType": "geometry",
+  "stylers": [{
+    "color": "#c9c9c9"
+  }]
+}, {
+  "featureType": "water",
+  "elementType": "labels.text.fill",
+  "stylers": [{
+    "color": "#9e9e9e"
+  }]
+}];
+
 function initMap() {
-  const myCoordinates = { lat: 41.878113, lng: -87.629799 };
-
-  //Search bounds local context
-  // let bigBounds = {
-  //   north: 42.3256,
-  //   south: 41.7397,
-  //   west: -88.2040,
-  //   east: -87.4548,
-  // };
-
-  const lcMapView = new google.maps.localContext.LocalContextMapView({
-    element: document.querySelector('#map'),
-    placeTypePreferences: [
-      'bakery', 
-      'cafe', 
-      'restaurant'
-    ],
-    maxPlaceCount: 24,
-    // locationRestriction: bigBounds,
-    directionsOptions: { origin: myCoordinates }
+  // Generate the placeholder map
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 0, lng: 0 },
+    zoom: 2,
+    disableDefaultUI: true,
   });
+  map.setOptions({ styles: silverStyle });
 
-  const map = lcMapView.map;
-  map.setOptions({
-    center: myCoordinates,
-    zoom: 16
-  });
-
-  const input = document.getElementById("autocomplete");
+  // Build and add the Autocomplete search bar
+  const input = document.getElementById('autocomplete');
   const options = {
-    types: ["establishment"],
-    componentRestrictions: { country: "us" }
-  };
-  const autocomplete = new google.maps.places.Autocomplete(input, options);
-  // Bind the map's bounds (viewport) property to the autocomplete object, so that the autocomplete requests use the current map bounds for the bounds option in the request.
-  autocomplete.bindTo("bounds", map);
-  // Set the data fields to return when the user selects a place.
-  autocomplete.setFields(
-    [
-      "address_components", 
-      "geometry", 
-      "icon", 
-      "name"
-    ]
-  );
-
-  //Infowindow configuration
-  const infowindow = new google.maps.InfoWindow();
-  const infowindowContent = document.getElementById("infowindow-content");
-  infowindow.setContent(infowindowContent);
-
-  const marker = new google.maps.Marker(
-    {
-      map,
-      anchorPoint: new google.maps.Point(0, -29),
+    types: ['address'],
+    componentRestrictions: {
+      country: 'us'
     }
-  );
+  };
 
-  autocomplete.addListener("place_changed", () => {
-    infowindow.close();
-    marker.setVisible(false);
+  const autocomplete = new google.maps.places.Autocomplete(input, options);
+  autocomplete.setFields(['address_components', 'geometry', 'name']);
+
+  // Load a Local Context map when the user selects an address
+  let originLocation = { lat: 37.402105, lng: -122.081974 };
+
+  autocomplete.addListener('place_changed', async () => {
     const place = autocomplete.getPlace();
 
     if (!place.geometry) {
       // User entered the name of a Place that was not suggested and
       // pressed the Enter key, or the Place Details request failed.
-      window.alert("No details available for input: '" + place.name + "'");
+      window.alert('No address available for that input.');
       return;
     }
 
-    // If the place has a geometry, then present it on a map.
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17); // Why 17? Because it looks good.
-    }
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
-    let address = "";
+    // Recenter the map to the selected address
+    originLocation = place.geometry.location;
+    console.log(place);
 
-    if (place.address_components) {
-      address = [
-        (place.address_components[0] &&
-          place.address_components[0].short_name) ||
-        "",
-        (place.address_components[1] &&
-          place.address_components[1].short_name) ||
-        "",
-        (place.address_components[2] &&
-          place.address_components[2].short_name) ||
-        "",
-      ].join(" ");
-    }
-    infowindowContent.children["place-icon"].src = place.icon;
-    infowindowContent.children["place-name"].textContent = place.name;
-    infowindowContent.children["place-address"].textContent = address;
-    infowindow.open(map, marker);
+    fillLocalContext(originLocation);
   });
 }
 
+function fillLocalContext(home) {
+  document.getElementById('map').innerHTML = '';
 
+  const localContextMapView = new google.maps.localContext.LocalContextMapView({
+    directionsOptions: { origin: home },
+    element: document.getElementById('map'),
+    placeTypePreferences: [
+      { type: 'bakery', weight: 2 },
+      { type: 'cafe', weight: 1 },
+      { type: 'restaurant', weight: 1 },
+      { type: 'supermarket', weight: 2 },
+    ],
+    maxPlaceCount: 24
+  });
 
+  map = localContextMapView.map;
 
+  map.setOptions({
+    center: home,
+    zoom: 16,
+  });
 
-// const searchUrl = "https://maps.googleapis.com/maps/api/js";
-// const apiKey = "O0bRxrneaYkbE13IJgdL9zcGKa20TqseF5gqaJkc";
-
-// function formatQueryParams(params) {
-//   const queryItems = Object.keys(params)
-//     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-//   return queryItems.join('&');
-// }
-
-// function generateRequestUrl(stateNames, maxResults) {
-//   let params = {
-//     v: 3.exp,
-//     key: apiKey,
-//     ca
-    
-//   }
-
-//   let queryString = formatQueryParams(params);
-//   return `${searchUrl}?${queryString}&stateCode=${stateNames.join()}`
-// }
-
-// function getNationalPark(stateNames, maxResults = 10) {
-//   let requestUrl = generateRequestUrl(stateNames, maxResults);
-//   fetch(requestUrl)
-//     .then(response => response.json())
-//     .then(responseJson => displayToDom(responseJson))
-// }
+  const originMarker = new google.maps.Marker({
+    position: home,
+    map: map,
+    icon: './home.png',
+    zIndex: 30,
+  });
+}
 
 function initialize() {
   initMap();
